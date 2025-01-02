7e3f5594748f2701cfa2f513dd4f606390f6d837
Hanhong Zhao <zhh24@mit.edu>
2025-01-02 05:38:16 +0000

    stupid env

--- code/solve_remote_env.sh
+++ code/solve_remote_env.sh
@@ -3,6 +3,12 @@ source ka.sh # import VM_NAME, ZONE
 echo 'solve'
 gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --worker=all \
     --command "
-pip3 install tensorstore==0.1.67
+pip list | grep jax
+pip list | grep orbax
+pip list | grep optree
+pip list | grep dtypes
 " # &> /dev/null
 echo 'solved!'
+
+
+# pip install orbax-checkpoint==0.6.4 optree==0.13.0
