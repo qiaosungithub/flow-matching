8aa5ab7847491dd2f29c2ba708994d6d9e5f15b9
jzc <jzc_2007@mit.edu>
2025-01-04 07:46:02 +0000

    change back to best config

--- code/装牛牛X.sh
+++ code/装牛牛X.sh
@@ -4,4 +4,4 @@ pip install jax[tpu]==0.4.27 -f https://storage.googleapis.com/jax-releases/libt
 pip install jaxlib==0.4.27 "flax>=0.8"
 # pip install -r requirements.txt # other tang dependencies
 pip install pillow clu tensorflow==2.15.0 "keras<3" "torch<=2.4" torchvision wandb tensorflow_datasets matplotlib==3.9.2
-pip install orbax-checkpoint==0.4.4 ml-dtypes==0.5.0 tensorstore==0.1.67
+pip install orbax-checkpoint==0.6.4 ml-dtypes==0.5.0 tensorstore==0.1.67
