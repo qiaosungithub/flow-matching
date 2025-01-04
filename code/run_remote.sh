d8451767a527636fc5b54031bd40d0ebf1a3312f
jzc <jzc_2007@mit.edu>
2025-01-04 02:06:42 +0000

    fix bash

--- code/run_remote.sh
+++ code/run_remote.sh
@@ -47,16 +47,16 @@ python3 main.py \
 }
 
 command
-for i in {1..3}; do
-    if grep -q 'Additional GRPC error information' $LOGDIR/output.log; then
-        echo 'Get GRPC error, retrying...'
-        # echo > $LOGDIR/output.log
-        mv $LOGDIR/output.log $LOGDIR/output.log.$i
-        bash re_apply.sh
-        command
-    else
-        break
-    fi
-done
+# for i in {1..3}; do
+#     if grep -q 'Additional GRPC error information' $LOGDIR/output.log; then
+#         echo 'Get GRPC error, retrying...'
+#         # echo > $LOGDIR/output.log
+#         mv $LOGDIR/output.log $LOGDIR/output.log.$i
+#         bash re_apply.sh
+#         command
+#     else
+#         break
+#     fi
+# done
 
 ############# No need to modify [END] #############
